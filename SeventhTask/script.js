64fcbac892a173ae96c791163c180dba8a24c5ad
s0188100student202421 <uliamaslaeva88@gmail.com>
2024-11-10 12:11:11 +0300

    Add files via upload

---
+++ SeventhTask/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    const totalImages = 8;
+
+    $('.slider').slick({
+        slidesToShow: 3, // Показывать 3 изображения
+        slidesToScroll: 3,
+        dots: true,
+        arrows: true, // Включаем стрелки навигации
+        prevArrow: '<button class="arrow left">&#10094;</button>', // Стрелка влево
+        nextArrow: '<button class="arrow right">&#10095;</button>', // Стрелка вправо
+        cssEase: 'ease',
+        responsive: [
+            {
+                breakpoint: 576, // Для мобильных устройств
+                settings: {
+                    slidesToShow: 1, // Показывать 1 изображение
+                    slidesToScroll: 1
+                    
+                }
+            }
+        ]
+    });
+});
