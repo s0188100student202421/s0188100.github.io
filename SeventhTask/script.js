4bf8a23ca652055714231944360bcd155e603024
s0188100student202421 <uliamaslaeva88@gmail.com>
2024-11-19 13:24:07 +0300

    Update script.js

--- SeventhTask/script.js
+++ SeventhTask/script.js
@@ -1,23 +1,26 @@
-$(document).ready(function() {
-    const totalImages = 8;
-
-    $('.slider').slick({
-        slidesToShow: 3, // Показывать 3 изображения
-        slidesToScroll: 3,
-        dots: true,
-        arrows: true, // Включаем стрелки навигации
-        prevArrow: '<button class="arrow left">&#10094;</button>', // Стрелка влево
-        nextArrow: '<button class="arrow right">&#10095;</button>', // Стрелка вправо
-        cssEase: 'ease',
-        responsive: [
-            {
-                breakpoint: 576, // Для мобильных устройств
-                settings: {
-                    slidesToShow: 1, // Показывать 1 изображение
-                    slidesToScroll: 1
-                    
+window.addEventListener('DOMContentLoaded', function (event) {
+  console.log("DOM fully loaded and parsed")
+    $(document).ready(function() {
+        const totalImages = 8;
+    
+        $('.slider').slick({
+            slidesToShow: 3, // Показывать 3 изображения
+            slidesToScroll: 3,
+            dots: true,
+            arrows: true, // Включаем стрелки навигации
+            prevArrow: '<button class="arrow left">&#10094;</button>', // Стрелка влево
+            nextArrow: '<button class="arrow right">&#10095;</button>', // Стрелка вправо
+            cssEase: 'ease',
+            responsive: [
+                {
+                    breakpoint: 576, // Для мобильных устройств
+                    settings: {
+                        slidesToShow: 1, // Показывать 1 изображение
+                        slidesToScroll: 1
+                        
+                    }
                 }
-            }
-        ]
+            ]
+        });
     });
 });
